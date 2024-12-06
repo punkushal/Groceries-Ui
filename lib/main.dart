7061f220bb10645cf1823ccea3ef1a00d86db0e0
punkushal <spycymagar@gmail.com>
2024-12-06 12:21:08 +0545

    Home Widget Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:grocerri_ui/screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,6 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
       ),
+      home: const SplashScreen(),
     );
   }
 }
